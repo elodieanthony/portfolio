9b9b52247a9bdcc9437adea91b474dfc648315ea
Elodie <anthonyelodie@gmail.com>
2023-04-10 17:42:40 +0200

    Add project details page

---
+++ src/views/ProjectDetails.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="project-detail">
+    <h1>Project Details</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectDetails',
+};
+</script>
+
+<style></style>
