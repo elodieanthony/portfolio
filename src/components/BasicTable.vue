006b69da235e0f047cc500af844a209c799bcf9c
Elodie <anthonyelodie@gmail.com>
2023-04-24 10:54:51 +0200

    Add table components

---
+++ src/components/BasicTable.vue
@@ -0,0 +1,58 @@
+<template>
+  <ul class="table">
+    <li
+      class="table__line"
+      v-for="(item, index) in tableItems"
+      :key="item.key"
+      :class="{ 'table__line--border-bottom': index >= tableItems.length - 1 }"
+      ref="itemElements"
+    >
+      <div class="table__title">
+        {{ item.title }}
+      </div>
+
+      <div class="table__content">{{ item.content }}</div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'TableProject',
+  props: {
+    tableItems: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.table {
+  &__line {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 1rem 0;
+    border-top: 1px solid black;
+    &--border-bottom {
+      border-bottom: 1px solid black;
+    }
+  }
+
+  &__title {
+    font-weight: 700;
+  }
+
+  &__date {
+    font-weight: 700;
+  }
+
+  &__text {
+    margin-top: 0;
+    transition: all 0.3s ease;
+  }
+}
+</style>
---
+++ src/components/TableProject.vue
@@ -0,0 +1,69 @@
+<template>
+  <ul class="table">
+    <li
+      class="table__line"
+      v-for="(item, index) in tableItems"
+      :key="item.key"
+      @click="goToProjectDetails(item.id)"
+      :class="{ 'table__line--border-bottom': index >= tableItems.length - 1 }"
+      ref="itemElements"
+    >
+      <div class="table__content">
+        <div class="table__title">
+          {{ item.title }}
+        </div>
+        <div class="table__text">
+          {{ item.text }}
+        </div>
+      </div>
+
+      <div class="table__date">{{ item.date }}</div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'TableProject',
+  props: {
+    tableItems: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    goToProjectDetails(id) {
+      this.$router.push({ name: 'projectDetails', params: { id: id } });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.table {
+  &__line {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 1rem 0;
+    border-top: 1px solid black;
+    &--border-bottom {
+      border-bottom: 1px solid black;
+    }
+  }
+
+  &__title {
+    font-weight: 700;
+  }
+
+  &__date {
+    font-weight: 700;
+  }
+
+  &__text {
+    margin-top: 0;
+    transition: all 0.3s ease;
+  }
+}
+</style>
